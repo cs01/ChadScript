8c50ff21df0a169ea570dc5e2ce537d32a9197f8
Chad Smith <cs01@users.noreply.github.com>
2026-01-30 17:32:41 -0800

    convert reference-express-server to typescript

--- tests/autonomous/reference-express-server.ts
+++ tests/autonomous/reference-express-server.ts
@@ -1,40 +1,40 @@
 // Express.js Reference Server for HTTP Parity Testing
 // This server provides the reference behavior that ChadScript HTTP server should match
 
-const express = require('express');
+import express, { Request, Response } from 'express';
 const app = express();
 
 app.use(express.text());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express!');
 });
 
-app.get('/echo', (req, res) => {
-  res.send(req.query.msg || '');
+app.get('/echo', (req: Request, res: Response) => {
+  res.send((req.query.msg as string) || '');
 });
 
-app.post('/echo', (req, res) => {
+app.post('/echo', (req: Request, res: Response) => {
   res.type('text/plain');
   res.send(req.body);
 });
 
-app.get('/json', (req, res) => {
+app.get('/json', (req: Request, res: Response) => {
   res.json({ message: 'hello', count: 42 });
 });
 
-app.get('/status/:code', (req, res) => {
+app.get('/status/:code', (req: Request, res: Response) => {
   const code = parseInt(req.params.code, 10);
   res.status(code).send('Status ' + req.params.code);
 });
 
-app.get('/headers', (req, res) => {
+app.get('/headers', (req: Request, res: Response) => {
   res.set('X-Custom-Header', 'test-value');
   res.send('Headers set');
 });
 
-app.post('/data', (req, res) => {
+app.post('/data', (req: Request, res: Response) => {
   res.json({ received: req.body, method: 'POST' });
 });
 
