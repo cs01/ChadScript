bcef015a4250785a35b1c48b7f06e541f11b05a0
cs01 <cs01@users.noreply.github.com>
2026-03-06 23:08:00 -0800

    restore isnumber global variable handling dropped in rebase

--- src/codegen/llvm-generator.ts
+++ src/codegen/llvm-generator.ts
@@ -1918,6 +1918,7 @@ export class LLVMGenerator extends BaseGenerator implements IGeneratorContext {
         let isClassInstance = false;
         let isUint8Array = false;
         let isBoolean = false;
+        let isNumber = false;
         if (resolved) {
           const base = resolved.base;
           const depth = resolved.arrayDepth;
@@ -1930,6 +1931,7 @@ export class LLVMGenerator extends BaseGenerator implements IGeneratorContext {
           isRegex = base === "RegExp";
           isObject = base === "object" && depth === 0;
           isBoolean = base === "boolean" && depth === 0;
+          isNumber = base === "number" && depth === 0;
           isUint8Array = base === "Uint8Array" && depth === 0;
           isClassInstance =
             !isRegex &&
@@ -2165,6 +2167,10 @@ export class LLVMGenerator extends BaseGenerator implements IGeneratorContext {
         } else if (isBoolean) {
           llvmType = "double";
           kind = SymbolKind.Boolean;
+          defaultValue = "0.0";
+        } else if (isNumber) {
+          llvmType = "double";
+          kind = SymbolKind.Number;
           defaultValue = "0.0";
         } else if (isJSONParse) {
           const interfaceName = this.typeInference.getJSONParseInterface(
