f5b99314f587ed60fdd889ed1d3e08d2cf685250
cs01 <cs01@users.noreply.github.com>
2026-03-11 21:42:45 -0700

    fix formatting

--- src/codegen/infrastructure/generator-context.ts
+++ src/codegen/infrastructure/generator-context.ts
@@ -1699,7 +1699,8 @@ export class MockGeneratorContext implements IGeneratorContext {
     doGenerateToUpperCase: (_strPtr: string): string => "%0",
     doGenerateToLowerCase: (_strPtr: string): string => "%0",
     doGenerateIndexOf: (_strPtr: string, _substring: string): string => "%0",
-    doGenerateIndexOfFrom: (_strPtr: string, _substring: string, _fromIndex: string): string => "%0",
+    doGenerateIndexOfFrom: (_strPtr: string, _substring: string, _fromIndex: string): string =>
+      "%0",
     doGenerateLastIndexOf: (_strPtr: string, _substring: string): string => "%0",
     doGenerateIncludes: (_strPtr: string, _substring: string): string => "%0",
     doGenerateSlice: (_strPtr: string, _start: string, _end: string | null): string => "%0",
