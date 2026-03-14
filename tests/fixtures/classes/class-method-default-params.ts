ac5fbf024dbe2aaf4a176deefb1b4f2fc43fd7ea
cs01 <cs01@users.noreply.github.com>
2026-03-14 02:49:51 -0700

    style: format test fixture

--- tests/fixtures/classes/class-method-default-params.ts
+++ tests/fixtures/classes/class-method-default-params.ts
@@ -22,14 +22,37 @@ const c: Calculator = new Calculator();
 
 let pass: boolean = true;
 
-if (g.greet() !== "hi world") { console.log("FAIL greet()"); pass = false; }
-if (g.greet("chad") !== "hi chad") { console.log("FAIL greet(chad)"); pass = false; }
+if (g.greet() !== "hi world") {
+  console.log("FAIL greet()");
+  pass = false;
+}
+if (g.greet("chad") !== "hi chad") {
+  console.log("FAIL greet(chad)");
+  pass = false;
+}
 
-if (f.format("x") !== "[x]") { console.log("FAIL format(x)"); pass = false; }
-if (f.format("x", "<") !== "<x]") { console.log("FAIL format(x, <)"); pass = false; }
-if (f.format("x", "<", ">") !== "<x>") { console.log("FAIL format(x, <, >)"); pass = false; }
+if (f.format("x") !== "[x]") {
+  console.log("FAIL format(x)");
+  pass = false;
+}
+if (f.format("x", "<") !== "<x]") {
+  console.log("FAIL format(x, <)");
+  pass = false;
+}
+if (f.format("x", "<", ">") !== "<x>") {
+  console.log("FAIL format(x, <, >)");
+  pass = false;
+}
 
-if (c.add(5) !== 15) { console.log("FAIL add(5)"); pass = false; }
-if (c.add(5, 20) !== 25) { console.log("FAIL add(5, 20)"); pass = false; }
+if (c.add(5) !== 15) {
+  console.log("FAIL add(5)");
+  pass = false;
+}
+if (c.add(5, 20) !== 25) {
+  console.log("FAIL add(5, 20)");
+  pass = false;
+}
 
-if (pass) { console.log("TEST_PASSED"); }
+if (pass) {
+  console.log("TEST_PASSED");
+}
