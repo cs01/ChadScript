f27675f3e670cd7b7ad39ef700a2ec7b24a387f3
Chad Smith <chadsmith.software@gmail.com>
2026-04-26 02:16:11 -0700

    [sema] reject arrow functions that capture this (#734)
    * [sema] add arrow-this-capture checker to prevent this usage in arrow functions
    
    * fix: remove typeof on union array elements for native compiler compat
    
    ---------
    
    Co-authored-by: cs01 <cs01@users.noreply.github.com>

--- src/codegen/llvm-generator.ts
+++ src/codegen/llvm-generator.ts
@@ -172,6 +172,7 @@ import { checkOrFallback } from "../semantic/or-fallback-checker.js";
 import { checkMapKeyTypes } from "../semantic/map-key-checker.js";
 import { checkMixedOperators } from "../semantic/mixed-operator-checker.js";
 import { checkDuplicateDeclarations } from "../semantic/duplicate-decl-checker.js";
+import { checkArrowThisCapture } from "../semantic/arrow-this-checker.js";
 import { checkMissingReturns, checkArgumentCounts } from "../semantic/safety-checks.js";
 import { DebugMetadataBuilder } from "./infrastructure/debug-metadata.js";
 
@@ -3139,6 +3140,7 @@ export class LLVMGenerator extends BaseGenerator implements IGeneratorContext {
     checkMixedOperators(this.ast, this.sourceCode);
     checkDuplicateDeclarations(this.ast, this.sourceCode);
     checkMapKeyTypes(this.ast, this.sourceCode);
+    checkArrowThisCapture(this.ast, this.sourceCode);
     this.stackEligibleVars = analyzeEscapes(this.ast);
     markIntSpecializedFunctions(this.ast);
     // Build pure-AST class + interface catalog so downstream queries
